d007405d3e26aea2154863b32ca8242b8da503f7
Ilya <iluhamishkin@gmail.com>
2022-11-14 22:12:42 +0100

    Add tailwind and sass to application

---
+++ tailwind.config.cjs
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    prefix: 'pw-',
+    content: [
+        './index.html',
+        './src/**/*.{js,ts,jsx,tsx}'
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+}
